eccdad5402515bfc4e09c741ed0211cda6fac2eb
kyonenya <kyon.musica@gmail.com>
2023-07-08 07:55:58 +0000

    feat: ローカルタイムゾーンに依存しないテスト

--- tests/dayjs.test.ts
+++ tests/dayjs.test.ts
@@ -1,13 +1,43 @@
 import { describe, it } from "node:test";
-import { equal, notEqual } from "node:assert/strict";
-import dayjs from "dayjs";
+import { equal, deepEqual, notEqual, notDeepEqual } from "node:assert/strict";
+import dayjs from "../dayjs";
 
 describe("dayjs", () => {
-  it("dayjsの日付文字列パースがローカルタイムゾーンに依存している", () => {
-    equal(process.env.TZ, undefined);
-    equal(dayjs("2023-07-08 04:52").format(), "2023-07-08T04:52:00+09:00");
+  it("`dayjs('withoutTimezone')` がすでにローカルタイムゾーンに依存している", () => {
+    process.env.TZ = "Asia/Tokyo";
+    deepEqual(dayjs("2023-07-08 04:52").$d, new Date("2023-07-07T19:52:00.000Z"));
     process.env.TZ = "UTC";
-    notEqual(dayjs("2023-07-08 04:52").format(), "2023-07-08T04:52:00+09:00"); // UTCだと失敗する
-    equal(dayjs("2023-07-08 04:52").format(), "2023-07-08T04:52:00+00:00");
+    notDeepEqual(dayjs("2023-07-08 04:52").$d, new Date("2023-07-07T19:52:00.000Z")); // UTCだと失敗する
+    deepEqual(dayjs("2023-07-08 04:52").$d, new Date("2023-07-08T04:52:00.000Z"));
+  });
+
+  it("`dayjs('withTimezone')` はローカルタイムゾーンに依存しない", () => {
+    process.env.TZ = "Asia/Tokyo";
+    // ISOString
+    deepEqual(dayjs("2023-07-08T04:52:00+09:00").$d, new Date("2023-07-07T19:52:00.000Z"));
+    process.env.TZ = "UTC";
+    deepEqual(dayjs("2023-07-08T04:52:00+09:00").$d, new Date("2023-07-07T19:52:00.000Z"));
+  });
+
+  it("`.format()` がローカルタイムゾーンに依存している", () => {
+    process.env.TZ = "Asia/Tokyo";
+    const date = dayjs("2023-07-08 04:52");
+    equal(date.format(), "2023-07-08T04:52:00+09:00");
+    process.env.TZ = "UTC";
+    notEqual(date.format(), "2023-07-08T04:52:00+09:00"); // UTCだと失敗する
+    equal(date.format(), "2023-07-08T04:52:00+00:00");
+  });
+  
+  it("`.tz().format()` はローカルタイムゾーンに依存しない", () => {
+    const date1 = dayjs("2023-07-08T04:52:00+09:00");
+    const date2 = dayjs("2023-07-07T19:52:00Z");
+    
+    process.env.TZ = "Asia/Tokyo";
+    equal(date1.tz().format(), "2023-07-08T04:52:00+09:00");
+    equal(date2.tz().format(), "2023-07-08T04:52:00+09:00");
+
+    process.env.TZ = "UTC";
+    equal(date1.tz().format(), "2023-07-08T04:52:00+09:00");
+    equal(date2.tz().format(), "2023-07-08T04:52:00+09:00");
   });
 });
